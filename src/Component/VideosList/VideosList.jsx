db5ef6b94b0ea137a978baf006961341540530f3
Bothianakhedr <Bothianakhedr198@gmail.com>
2025-02-17 18:13:16 +0200

    images

--- src/Component/VideosList/VideosList.jsx
+++ src/Component/VideosList/VideosList.jsx
@@ -31,29 +31,30 @@ export default function VideosList() {
         "https://yt3.ggpht.com/9wWwue3IsHkJ_nQpvPS5D5f4vJNkyVoxC4B77qpWtZXE9zi3ykWcO2hFQDOPZjGtVTMlRhL-=s68-c-k-c0x00ffffff-no-rj",
     },
     {
-      title: ` افضل 5 كتب قريتها هتغير حياتك في 2025`,
-      channel: "ahmed abdelhady",
-      views: "25K views ",
+      title: `React Level 1 - Part 1 :ReactJS Hooks 2023  `,
+      channel: "kimz-codes",
+      views: " 27K views",
       channelImage:
-        "https://i.ytimg.com/vi/pQZIBllJGgQ/hqdefault_6166.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCNUbF5e_IB3jcybdvrLWwZbyfG0g",
+        "https://i.ytimg.com/vi/p8nOfP2fw24/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAqES7yWle_EWKXi_zhFwvSRkWU6w",
       imageIcon:
-        "https://yt3.googleusercontent.com/UGcuE6niVr5GTJ6W_bqKltzDd2WYI041p5bXufQSionkuquqpE1_w22ZmfMmhFvyjTBe1YKi9A=s160-c-k-c0x00ffffff-no-rj",
+        "https://yt3.ggpht.com/lwoqnst1EoguIdFK35nSHrN0hIWieJvRHGSc4AYStd74MLNT42-4K6S-9P4PIRWQaQNz2xHDbA=s68-c-k-c0x00ffffff-no-rj",
     },
-    {
-      title: ` خارطه تعلم البرمجه`,
-      channel: "mohamed abohahoud",
-      views: "25K views",
-      "channelImage":
-        "https://i.ytimg.com/vi/ZJ81jSdoFKI/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC7lwfbagq99ks51o7zLPsarb1FFg",
-      "imageIcon":
-        "https://yt3.googleusercontent.com/q3PilZ2e7enLTBdKbMmMoBxyuwtfVull13jiFuY3RKky0f4iw-VrAjOEh3ojMjRqHl6YusxwI8w=s160-c-k-c0x00ffffff-no-rj",
-    },
+    
     {
       title: ` رتب مشاعرك وارفع وعيك وتخلص من كرهك لذاتك والاخرين  بهذه الممارسات`,
       channel: "sara",
       views: "1.M views",
       channelImage: "https://i.ytimg.com/vi/IRVTv6ZYzG0/hqdefault.jpg?sqp=-oaymwEnCOADEI4CSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDX18Hl2Ggh97uiIMHAOG3c9rBEdA",
       imageIcon: "https://yt3.ggpht.com/ytc/AIdro_nAv5Yz5b0p3HlRQe_A7jquqHOzxhzJT6MpupXVL8XhLcQ=s68-c-k-c0x00ffffff-no-rj",
+    },
+    {
+      title: `React Level 1 - Part 1 :ReactJS Hooks 2023  `,
+      channel: "kimz-codes",
+      views: " 27K views",
+      channelImage:
+        "https://i.ytimg.com/vi/p8nOfP2fw24/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAqES7yWle_EWKXi_zhFwvSRkWU6w",
+      imageIcon:
+        "https://yt3.ggpht.com/lwoqnst1EoguIdFK35nSHrN0hIWieJvRHGSc4AYStd74MLNT42-4K6S-9P4PIRWQaQNz2xHDbA=s68-c-k-c0x00ffffff-no-rj",
     },
   ]);
   return (
